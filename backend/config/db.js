0ae70e12f501540d24a95a7ce0cffb319358fda1
Sneha Chourey <snehachourey@gmail.com>
2026-06-16 13:38:04 +0530

    setup mongodb atlas connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 const connectDB=async()=>{
+    // console.log(process.env.MONGO_URI);
     try{
         const connect = await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected successfully');
     }
     catch(error){
-        console.log('MongoDB connection failed:',error.message);
+        console.log('MongoDB connection failed:',error);
         process.exit(1);
 
     }
